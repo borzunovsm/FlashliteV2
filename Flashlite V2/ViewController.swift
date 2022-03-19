48bc5336f4d327cf803c9c0ed2075aebd4b35894
Serega <borzunov_sm2mail.ru>
2022-03-19 14:55:01 +0300

    Initial Commit

---
+++ Flashlite V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlite V2
+//
+//  Created by Serega on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
